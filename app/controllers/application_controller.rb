598d3756afe75402dc5622162f4b84c0388d9b57
Max <xamskier@aol.com>
2018-04-10 14:24:19 -0400

    working scraper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 class ApplicationController < ActionController::Base
-  include JSONAPI::ActsAsResourceController
+  # include JSONAPI::ActsAsResourceController
   protect_from_forgery with: :exception
+
+  class Page
+    def initialize(title, link)
+      @title = title
+      @link = link
+    end
+    attr_reader :title
+    attr_reader :link
+  end
+
+  def scrape_page
+    require 'open-uri'
+    document = Nokogiri::HTML(open("https://www.reddit.com/"))
+
+    pages = document.css('.entry')
+    @pages_arr = []
+    pages.each do |entry|
+      title = entry.css('p.title>a').text
+      link = entry.css('p.title>a')[0]['href']
+      @pages_arr << Page.new(title, link)
+    end
+
+    render template: 'scrape_page'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'application#scrape_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  jsonapi_resources :pages do
-    jsonapi_related_resources :headers
-    jsonapi_related_resources :links
-  end
+  # jsonapi_resources :pages do
+  #   jsonapi_related_resources :headers
+  #   jsonapi_related_resources :links
+  # end
 end
